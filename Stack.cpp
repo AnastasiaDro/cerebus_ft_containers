c8022a1e8ac5e3b2ca886d47b50edbaac2cf5fde
cerebus <adrogunova@gmail.com>
2022-05-10 17:57:58 +0300

    Starting stack development

---
+++ Stack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Cesar Erebus on 5/10/22.
+//
+
+#include "Stack.hpp"
---
+++ Stack.hpp
@@ -0,0 +1,23 @@
+//
+// Created by Cesar Erebus on 5/10/22.
+//
+
+#ifndef CEREBUS_FT_CONTAINERS_STACK_H
+#define CEREBUS_FT_CONTAINERS_STACK_H
+
+namespace ft {
+    class Stack {
+    private:
+
+    public:
+        typedef Cont container_type
+        Stack();
+    //нужен ли здесь второй конструктор
+        explicit Stack(const container_type& cont)
+
+    };
+}
+
+
+
+#endif //CEREBUS_FT_CONTAINERS_STACK_H
